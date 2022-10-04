bd7e174050e490061ebb33bcb7ba9c75b86ace7b
Rachana Kumbhakar <rachanakumbhakar2019@gmail.com>
2022-10-04 13:40:12 -0600

    new update

--- score.cs
+++ score.cs
@@ -4,6 +4,38 @@ using System.Collections.Generic;
 
 class score
 {
+    int totalScore;
+    public score()
+    {
+       totalScore = 300; 
+
+    }
+
+    public int AddScore(){
+        totalScore = totalScore + 100;
+        return totalScore;
+    }
+
+    public int LessScore(){
+
+        totalScore = totalScore - 75;
+        return totalScore;
+    }
+
+    public bool Score0(){
+        if (totalScore <= 0 ){
+            return true;
+
+        }
+        else{
+             return false;
+        }
+
+    }
+    public void print(){
+
+        Console.WriteLine(totalScore);
+    }
 
 
 }
