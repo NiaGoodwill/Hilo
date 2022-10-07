149b96c35afcf5dd43bdbc6652b7b8db6ba502a9
NiaGoodwill <goodneg11@gmail.com>
2022-10-07 13:58:00 -0600

    progress

--- Deck.cs
+++ Deck.cs
@@ -1,21 +1,97 @@
 class Deck
 {
-    List<string> cards = new List<string>();
+    List<string> deck = new List<string>();
+    Dictionary<string, int> deckInts = new Dictionary<string, int>();
+    List<string> suits = new List<string> {"Spades", "Hearts","Clubs", "Diamonds"};
     Random random = new Random();
     public Deck()
     {
+        ShuffleDeck();
+        CreateDict();
+    }
 
+    // replenish the deck. Remove all cards currently in the deck then add a full deck 
+    void CollectCards()
+    {
+        if ((deck.Count()) != 0)
+        {
+            for ( int i = deck.Count() -1; i >= 0; i--)
+                {
+                    deck.RemoveAt(i);
+                }
+        }
+
+        for (int i = 2; i <=10; i++)
+        {
+            deck.Add($"{i} of {suits[0]}");
+            deck.Add($"{i} of {suits[1]}");
+            deck.Add($"{i} of {suits[2]}");
+            deck.Add($"{i} of {suits[3]}");
+        }
+        foreach (string suit in suits)
+        {
+            deck.Add($"Ace of {suit}");
+            deck.Add($"Jack of {suit}");
+            deck.Add($"Queen of {suit}");
+            deck.Add($"King of {suit}");
+        }
     }
     
-    public string DrawCard()
+    // Draw, display, and discard the first card in the deck
+    public void DrawCard()
     {
-        return "";
+        Console.WriteLine($"{deck[0]}");
+        deck.RemoveAt(0);
     }
 
     public void ShuffleDeck()
     {
+        CollectCards();
+        List<string> tempDeck = deck;
+        for ( int i = deck.Count() -1; i >= 0; i--)
+        {
+        deck.RemoveAt(i);
+        }
+        while (tempDeck.Count() != 0)
+        {
+            int randomIndex = random.Next(tempDeck.Count());
+            deck.Add(tempDeck[randomIndex]);
+            tempDeck.RemoveAt(randomIndex);
+        }
+    }
 
+    // take two cards and give the position (in terms of value) of the second relative to the first, higher "h", lower "l" or the same "s"
+    public string RelativeCardPosition(string controlValueCard, string testCard)
+    {
+        if (deckInts[testCard] > deckInts[controlValueCard])
+        {
+            return "h";
+        }
+        else if (deckInts[testCard] < deckInts[controlValueCard])
+        {
+            return "l";
+        }
+        else
+        {
+            return "s";
+        }
+    }
+    // Used at the start. Creates the numerical dictionary to give value to every card
+    public void CreateDict()
+    {
+        for (int i = 2; i <=10; i++)
+        {
+            deckInts.Add($"{i} of {suits[0]}", i);
+            deckInts.Add($"{i} of {suits[1]}", i);
+            deckInts.Add($"{i} of {suits[2]}", i);
+            deckInts.Add($"{i} of {suits[3]}", i);
+        }
+        foreach (string suit in suits)
+        {
+            deckInts.Add($"Ace of {suit}", 1);
+            deckInts.Add($"Jack of {suit}", 11);
+            deckInts.Add($"Queen of {suit}", 12);
+            deckInts.Add($"King of {suit}", 13);
+        }
     }
 }
-
-// Description () returns "{name of card} of {suit}s"
